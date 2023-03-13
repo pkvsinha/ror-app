42108ee6047fd17796a066c43b3d74e15ba2fe7c
Prashant Sinha <prsinha@groupon.com>
2023-03-13 16:47:33 +0530

    init posts

---
+++ app/mailers/comments_mailer.rb
@@ -0,0 +1,14 @@
+class CommentsMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.comments_mailer.submitted.subject
+  #
+  def submitted(comment)
+    @greeting = "Hi"
+    @comment = comment
+
+    mail to: "pkv.sinha492@gmail.com", subject: "New Comment!"
+  end
+end
